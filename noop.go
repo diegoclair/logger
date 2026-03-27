77e563739d707102097682450f3f862aadea94c6
Diego Clair <diego93rodrigues@gmail.com>
2026-03-26 23:33:24 -0300

    feat: lint

--- noop.go
+++ noop.go
@@ -11,4 +11,4 @@ func (l *noopLogger) Error(ctx context.Context, msg string, fields ...Field)    
 func (l *noopLogger) Fatal(ctx context.Context, msg string, fields ...Field)    {}
 func (l *noopLogger) Critical(ctx context.Context, msg string, fields ...Field) {}
 func (l *noopLogger) Print(args ...any)                                         {}
-func (l *noopLogger) Printf(msg string, args ...any)                           {}
+func (l *noopLogger) Printf(msg string, args ...any)                            {}
